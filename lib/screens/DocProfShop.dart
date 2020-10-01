1f4b06f48ca8c97f143d6538f1f92a12076e8a93
ajmal110 <69468040+ajmal110@users.noreply.github.com>
2020-10-01 22:20:34 +0530

    added few sub categories and background for welcome screen and added a button turf in categories

--- lib/screens/DocProfShop.dart
+++ lib/screens/DocProfShop.dart
@@ -100,7 +100,6 @@ class DocProfShop extends StatelessWidget {
                         ),
                       ],
                     ),
-
                   ),
                 ),
               ),
